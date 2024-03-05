0b6037ff66f354c942c208c6de74e6542cc610a2
Manya Jain <manyajain62002@gmail.com>
2024-03-05 15:46:03 +0530

    minor css change

--- src/components/ReplyBox.tsx
+++ src/components/ReplyBox.tsx
@@ -3,7 +3,7 @@ import { VscSend } from "react-icons/vsc";
 
 const ReplyBox = () => {
   return (
-    <div className="bg-white w-full h-10 rounded-md flex flex-row justify-between items-center px-2">
+    <div className="bg-white w-full min-h-10 rounded-md flex flex-row justify-between items-center px-2">
       <input
         type="text"
         className="w-80% pl-2 outline-none text-sm"
